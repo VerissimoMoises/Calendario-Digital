cb94b82531e93edc17954ed0bdcc5b3ed38385cd
Moisés Verissimo de Souza <102416201+VerissimoMoises@users.noreply.github.com>
2023-01-24 19:35:31 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,11 @@ const relogio = setInterval(function time() {
     let min  = dateToday.getMinutes();
     let seg  = dateToday.getSeconds();
 
-    if (hora >= 5 && hora < 8) {
+    if (hora >= 6 && hora < 8) {
         document.body.style.background = 'linear-gradient(0deg, #e4731c, #df3f22, #415b96)';
     } else if (hora >= 8 && hora < 16) {
         document.body.style.background = 'linear-gradient(50deg, #3498db, #819dda, #e6b458)';
-    } else if (hora >= 16 && hora <= 19) {
+    } else if (hora >= 16 && hora < 19) {
         document.body.style.background = 'linear-gradient(180deg, #e4731c, #df3f22, #415b96)';
     } else {
         document.body.style.background = 'linear-gradient(#5d4366, #445b8d, #160821)';
